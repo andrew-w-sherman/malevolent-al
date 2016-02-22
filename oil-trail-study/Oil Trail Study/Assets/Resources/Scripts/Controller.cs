2253c194222f7128e0093941abec9f7457682650
Jmiller4 <jmiller4@oberlin.edu>
2016-02-22 17:30:51 -0500

    Added files via upload

---
+++ oil-trail-study/Oil Trail Study/Assets/Resources/Scripts/Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+    public OilPatch[] oilList;
+    GameObject oilBlobObject;
+	void Start () {
+        oilList = new OilPatch[10];
+
+        oilBlobObject = new GameObject();
+        oilBlobObject.tag = "ball";
+        BoxCollider2D coll = oilBlobObject.AddComponent<BoxCollider2D>();
+        coll.size = new Vector2(1, 1);
+        OilBall oilball = oilBlobObject.AddComponent<OilBall>();
+        oilball.transform.position = new Vector3(0, 0, 0);
+        oilball.init(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ oil-trail-study/Oil Trail Study/Assets/Resources/Scripts/OilPatch.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OilPatch : MonoBehaviour {
+
+    public float clock;
+    public float onFireTimer;
+    public bool onFire;
+    public bool spreading;
+    float spreadLimit;
+    float fireLimit;
+
+    OilBall b;
+    int indexIAm;
+    OilModel model;
+
+    public void init(OilBall b, int i)
+    {
+        this.b = b;
+        indexIAm = i;
+        var modelObject = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        model = modelObject.AddComponent<OilModel>();
+        model.init(false, null, this);
+
+        clock = 0f; onFireTimer = 0f;
+        onFire = false; spreading = false;
+        spreadLimit = 0.5f;
+        fireLimit = 2f;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnMouseUpAsButton()
+    {
+        setOnFire();
+    }
+
+    void setOnFire()
+    {
+        if (!onFire)
+        {
+            onFire = true;
+            model.putOnFire();
+        }
+    }
+    
+    void OnTriggerStay2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "spreading")
+        {
+            setOnFire();
+            /*
+            if (coll.gameObject.GetComponent<OilPatch>().spreading)
+            {
+                setOnFire();
+            }
+
+        */
+        }
+        if (spreading && coll.gameObject.tag == "ball")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        clock = clock + Time.deltaTime;
+        if (onFire)
+        {
+            onFireTimer += Time.deltaTime;
+            if (onFireTimer > spreadLimit)
+            {
+                spreading = true;
+                this.tag = "spreading";
+                if (onFireTimer > fireLimit + spreadLimit)
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+	}
+}
