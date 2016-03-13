df823dd9dcd13b546c1430a788b178965a62bd58
marshdog <dmarsh2@oberlin.edu>
2016-03-13 14:21:25 -0400

    Update dan tutorial1 to have spaces instead of OO

--- prototype/FireOilPrototype/Assets/Resources/Scripts/GameController.cs
+++ prototype/FireOilPrototype/Assets/Resources/Scripts/GameController.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour {
 
     public bool DEBUG_LVL;
-    readonly string[] LEVELS = { "dan tutorial1" };
+    readonly string[] LEVELS = { "dan tutorial2" };
     int levelIndex;
 
     public GameObject boardGO;
