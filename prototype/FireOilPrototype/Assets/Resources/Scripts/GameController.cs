bf9028e593d67dda05b07547ad5f3e1a56166887
marshdog <dmarsh2@oberlin.edu>
2016-03-15 05:19:12 -0400

    Push it to the limit

--- prototype/FireOilPrototype/Assets/Resources/Scripts/GameController.cs
+++ prototype/FireOilPrototype/Assets/Resources/Scripts/GameController.cs
@@ -8,7 +8,7 @@ public class GameController : MonoBehaviour {
     public bool DEBUG_LVL = false;
     public bool DEBUG_BOSS = false;
 
-	readonly string[] LEVELS = { "test", "test2", "dan tutorial2","dan tutorial3", "dan tutorial4", "dan tutorial1", "joel1", "dan level", "bossLevel" };
+	readonly string[] LEVELS = { "test", "test2", "dan tutorial2","dan tutorial3", "dan tutorial4", "dan tutorial1", "dan level 1", "joel1", "dan level", "bossLevel" };
 	//"joel1"
     int levelIndex;
 
